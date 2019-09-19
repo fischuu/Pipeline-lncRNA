b4c6447c1da42217f179ed38c5bbe33ba7fc25c8
fischuu <github@danielfischer.name>
2019-09-19 20:05:51 +0300

    Typo fixed

--- applyLNCPipePuhti_equus_caballus.sh
+++ applyLNCPipePuhti_equus_caballus.sh
@@ -10,4 +10,4 @@ snakemake -s applyLNCPipe.smk \
           --latency-wait 60 \
           --configfile /scratch/project_2001289/FAANG_lncRNA/pipeline/applyLNCPipe_config_puhti_equus_caballus.yaml \
           --cluster-config applyLNCPipe_puhti.yaml \
-          --cluster "sbatch -t {cluster.time} --acount={cluster.account} --job-name={cluster.job-name} --tasks-per-node={cluster.ntasks} --cpus-per-task={cluster.cpus-per-task} --mem-per-cpu={cluster.mem-per-cpu} -p {cluster.partition} -D {cluster.working-directory}" $1 
+          --cluster "sbatch -t {cluster.time} --account={cluster.account} --job-name={cluster.job-name} --tasks-per-node={cluster.ntasks} --cpus-per-task={cluster.cpus-per-task} --mem-per-cpu={cluster.mem-per-cpu} -p {cluster.partition} -D {cluster.working-directory}" $1 
